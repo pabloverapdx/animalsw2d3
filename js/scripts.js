ca4e93ce43d21d26b8a55bf649d9358ae3830c00
jackson cafazzo and Pablo Vera <pablovera.pdx@gmail.com>
2016-01-13 09:08:20 -0800

    add bird section and spelling alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  var choice = prompt("Would you like to learn about dogs or cats?");
+  var choice = prompt("Would you like to learn about dogs, cats, or birds?");
 
   if (choice === 'dogs') {
     $('#dogs').show();
-  } else {
+  }
+  if (choice === 'cats') {
     $('#cats').show();
+  }
+  if (choice === 'birds') {
+    $('#birds').show();
+      }
+  else {
+    alert("check your spelling")
   }
 });
